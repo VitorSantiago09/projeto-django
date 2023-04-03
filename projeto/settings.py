e41b0a6510e1523cb0587f6c90508b85146b9bf0
Vitor Santiago Pereira <vspereira@travelexbank.com.br>
2023-04-03 11:19:00 -0300

    'att

--- projeto/settings.py
+++ projeto/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    #INFORMAR OS APPS TAMBÉM
+    "recipes",
 ]
 
 MIDDLEWARE = [
