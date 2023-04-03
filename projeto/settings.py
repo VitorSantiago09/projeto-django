e02aa740c32b38c66aaa36996cc4b1d02b55847b
Vitor Santiago Pereira <vspereira@travelexbank.com.br>
2023-04-03 09:12:45 -0300

    'att'

--- projeto/settings.py
+++ projeto/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-$x30%^ie&rz8v$tglhhphgq_xg1^at(i$wbq-h%_gndj)2q2@o"
+SECRET_KEY = "django-insecure-(auu-fh!05*jblosp&zcpu3(oin0zl^0))c!v$p1u!i$mkk4u("
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
