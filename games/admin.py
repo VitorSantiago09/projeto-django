3f42ef5bf37b3da7ea4ba94a96d9f0d11ca3b048
Vitor Santiago Pereira <vspereira@travelexbank.com.br>
2023-04-04 13:54:29 -0300

    'att'

--- games/admin.py
+++ games/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from games.models import Category, Jogo
 
 # Register your models here.
+
+#Registramos os models aqui para que eles apareçam na parte de admin do django para futuras edições
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Jogo)
+class JogosAdmin(admin.ModelAdmin):
+    pass
+
--- games/models.py
+++ games/models.py
@@ -1,18 +1,22 @@
 from unicodedata import category
 from django.db import models
 from django.contrib.auth.models import User
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 # Create your models here.
 
 class Category(models.Model):
     name = models.CharField(max_length=100)
+    #Isso tira a nomenclatura padrão do nome da tabela que vem no admin do django quando criamos uma coluna. Com isso o nome da coluna vem com o nome que selecionamos.
+    def __str__(self):
+        return self.name
 
 #Cada Model será uma tabela dentro do nosso banco de dados
-class Jogos(models.Model):
+class Jogo(models.Model):
     titulo = models.CharField(max_length=100)
     descricao = models.TextField()
     genero = models.CharField(max_length=50)
-    nota_metracritic = models.IntegerField()
+    nota_metracritic = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(100)])
     capa_jogo = models.ImageField(upload_to='games/covers')
     category = models.ForeignKey(
         Category, on_delete=models.SET_NULL, null=True
